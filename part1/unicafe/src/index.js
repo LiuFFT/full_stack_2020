8034ee911e4bd90894dfac8e2fe5b1b10a6b90bc
zhenkunliu <liufft98@gmail.com>
2020-12-21 17:00:54 +0800

    part1-unicafe

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => {
+    // save clicks of each button to own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    return (
+        <div>
+            code here
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals

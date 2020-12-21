cfaa449b9ea9a1dea69eb5754f8953e27ba1fa9b
zhenkunliu <liufft98@gmail.com>
2020-12-21 21:11:26 +0800

    part2-exercises2.2-change last line

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -9,7 +9,7 @@ const Header = ({ course }) => {
 
 const Total = ({ course }) => {
     return(
-        <p>Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises + course.parts[3].exercises}</p>
+        <p>total of {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises + course.parts[3].exercises} exercises</p>
     )
 }
 

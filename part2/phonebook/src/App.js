1ee96e88784c3defba9ac01fc9ee86abc83fb154
zhenkunliu <liufft98@gmail.com>
2020-12-22 16:18:59 +0800

    part2-exercises2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Number from "./components/Number";
+import axios from 'axios'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
+        // { name: 'Arto Hellas', number: '040-123456' },
+        // { name: 'Ada Lovelace', number: '39-44-5323523' },
+        // { name: 'Dan Abramov', number: '12-43-234345' },
+        // { name: 'Mary Poppendieck', number: '39-23-6423122' }
     ])
     const [ newName, setNewName ] = useState('')
     const [ newNumber, setNewNumber ] = useState('')
-    // const [showAll, setShowAll] = useState(true)
     const [ filter, setNewFilter ] = useState('')
 
     // const personToShow = (event) =>{
@@ -22,6 +22,17 @@ const App = () => {
     //     setPersons(copy)
     //     // setNewFilter('')
     // }
+
+    useEffect(() => {
+        console.log('effect')
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                console.log('promise fulfilled')
+                setPersons(response.data)
+            })
+    }, [])
+    console.log('render', persons.length, 'persons')
 
     const filterPerson = persons.filter((person) => person.name.toLowerCase().includes(filter.toLowerCase()))
 

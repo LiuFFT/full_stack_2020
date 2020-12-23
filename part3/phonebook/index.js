4e7c06b202b929dbc13912ca5294282124a6444a
zhenkunliu <liufft98@gmail.com>
2020-12-23 18:15:58 +0800

    part3-exercises3.6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -90,9 +90,17 @@ const generateId = () => {
 app.post('/api/persons', (request, response) => {
     const body = request.body
 
+    // const uid = generateId()
+
+    const personExist = persons.find(person => person.name === body.name)
+
     if (!body.name || !body.number) {
         return response.status(400).json({
             error: 'name or number missing'
+        })
+    }else if (personExist){
+        return response.status(400).json({
+            error: `name must be unique, ${body.name} already exist`
         })
     }
 

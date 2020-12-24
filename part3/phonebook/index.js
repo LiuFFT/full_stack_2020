1cfe85fb0d314d5b748297e13b271580e12cb330
zhenkunliu <liufft98@gmail.com>
2020-12-24 17:16:15 +0800

    part3-exercises3.17 & 3.18

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -89,6 +89,21 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
 

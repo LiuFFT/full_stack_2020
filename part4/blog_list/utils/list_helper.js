6a02844358f29cf7deadfcd41293711d775164b6
zhenkunliu <liufft98@gmail.com>
2020-12-25 16:11:37 +0800

    part4-exercises4.4

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -3,6 +3,15 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const likes_list = blogs.map(blog => blog.likes)
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    const likes = likes_list.reduce(reducer)
+
+    return likes
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }

f723db4a474ece542b21173f19f7393748789631
zhenkunliu <liufft98@gmail.com>
2020-12-25 10:07:53 +0800

    part3-exercises3.22

---
+++ part3/phonebook/README.md
@@ -0,0 +1,18 @@
+# Full Stack Open 2020—Part3
+
+Solutions for exercises of part 3.
+
+
+
+## Deployment
+
+URL for heroku apps.
+
+### Deployment For exercises 3-10
+
+https://fso-3-10.herokuapp.com/
+
+### Deployment For exercises 3-21
+
+https://fso-3-21.herokuapp.com/
+
--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const uniqueValidator = require('mongoose-unique-validator');
+const uniqueValidator = require('mongoose-unique-validator')
 
 
 const url = process.env.MONGODB_URI
@@ -7,35 +7,36 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+// eslint-disable-next-line no-unused-vars
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 3,
-        required: true,
-        unique: true
-    },
-    number: {
-        type: String,
-        minlength: 8,
-        required: true
-    },
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+    unique: true
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    required: true
+  },
 })
 
 // const Person = mongoose.model('Person', personSchema)
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 personSchema.plugin(uniqueValidator)

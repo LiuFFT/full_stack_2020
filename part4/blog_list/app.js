40821008bc829027593dba0b123ceaecd807f317
zhenkunliu <liufft98@gmail.com>
2020-12-28 10:11:21 +0800

    part4-exercises4.8

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -1,5 +1,6 @@
 const config = require('./utils/config')
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
@@ -17,10 +18,10 @@ logger.info('connecting to', config.MONGODB_URI)
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
     // eslint-disable-next-line no-unused-vars
     .then(result => {
-        console.log('connected to MongoDB')
+        logger.info('connected to MongoDB')
     })
     .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
+        logger.info('error connecting to MongoDB:', error.message)
     })
 
 app.use(cors())
---
+++ part4/blog_list/tests/blogList_api.test.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
+})
+
+describe('blogs list tests', () => {
+
+    test('blogs number is correct', async () => {
+        const response = await api.get('/api/blogs')
+
+        expect(response.body.length).toBe(helper.initialBlogs.length)
+    })
+
+})
+
+
+afterAll(() => {
+    mongoose.connection.close()
+})

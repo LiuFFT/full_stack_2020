cfd985e07fbb9f01bd4da61870a24e71ba539b5c
zhenkunliu <liufft98@gmail.com>
2021-01-03 16:49:20 +0800

    part5-exercises5.11

--- part5/bloglist-frontend/src/components/Togglable.js
+++ part5/bloglist-frontend/src/components/Togglable.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+
 
 const Togglable = (props) => {
     const [visible, setVisible] = useState(false)
@@ -21,6 +23,10 @@ const Togglable = (props) => {
             </div>
         </div>
     )
+}
+
+Togglable.propTypes = {
+    buttonLabel: PropTypes.string.isRequired
 }
 
 export default Togglable

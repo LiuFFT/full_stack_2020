da9036ae016c264e423e56628828abe9b34d2434
zhenkunliu <liufft98@gmail.com>
2021-01-03 09:35:54 +0800

    part5-exercises5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,31 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title}, {blog.author}
-  </div>
+import React,{useState} from 'react'
+import Button from "./Button";
+
+const BlogDetail = ({blog}) => (
+    <div>
+        {blog.url}<br/>
+        likes {blog.likes} <Button text="like"/><br/>
+        {blog.user.username || null}<br/>
+    </div>
 )
+
+
+const Blog = ({ blog }) => {
+
+    const [show, setShow] = useState(false)
+
+    const handleClick = () =>{
+        setShow(!show)
+    }
+
+    return (
+        <div>
+            {blog.title}, {blog.author} <Button onClick={handleClick} text="show"/>
+            <div>
+                {show ? <BlogDetail blog={blog}/> : ''}
+            </div>
+        </div>
+    )
+}
 
 export default Blog
